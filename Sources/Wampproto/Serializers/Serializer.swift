acfc69ebd6b03369a5aba84dcb9c5142e4a7311e
Muzzammil Shahid <muzzammil@xconn.io>
2025-02-06 13:50:40 +0500

    Add Serializer protocol and helpers

---
+++ Sources/Wampproto/Serializers/Serializer.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+protocol Serializer {
+    func serialize(message: Message) throws -> Any
+    func deserialize(data: Any) throws -> Message
+}
+
+enum SerializerError: Error {
+    case serializationError(String)
+    case deserializationError(String)
+    case invalidMessageFormat
+}
+
+enum MessageParsingError: Error {
+    case unsupportedType(Int)
+    case parseFailure(String)
+}
+
+func toMessage(data: [Any]) throws -> Message {
+    guard let type = data.first as? Int else {
+        throw MessageParsingError.parseFailure("Data is not in the expected format")
+    }
+
+    switch type {
+    case Hello.id:
+        return try Hello.parse(message: data)
+    default:
+        throw MessageParsingError.unsupportedType(type)
+    }
+}
