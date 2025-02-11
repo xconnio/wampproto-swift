eb40f135be711ffc067df0d04b8e2d2c7344ca9d
Muzzammil Shahid <muzzammil@xconn.io>
2025-02-11 19:31:41 +0500

    Add BinaryPayload protocol

--- Sources/Wampproto/Messages/Message.swift
+++ Sources/Wampproto/Messages/Message.swift
@@ -5,3 +5,9 @@ protocol Message {
     static func parse(message: [Any])throws -> Message
     func marshal() -> [Any]
 }
+
+protocol BinaryPayload {
+    var payloadIsBinary: Bool { get }
+    var payload: Data? { get }
+    var payloadSerializer: Int { get }
+}
