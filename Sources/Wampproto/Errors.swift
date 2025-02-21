76b74f5a5f8bdbff37ddc5fa12e973bb2fd0b1b3
Muzzammil Shahid <muzzammil@xconn.io>
2025-02-21 16:51:50 +0500

    Implement WAMP session joiner

---
+++ Sources/Wampproto/Errors.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class ApplicationError: Swift.Error, CustomStringConvertible {
+    let message: String
+    let args: [Any]?
+    let kwargs: [String: Any]?
+
+    init(message: String, args: [Any]? = nil, kwargs: [String: Any]? = nil) {
+        self.message = message
+        self.args = args
+        self.kwargs = kwargs
+    }
+
+    var description: String {
+        var errStr = message
+        if let args = args, !args.isEmpty {
+            let argsStr = args.map { "\($0)" }.joined(separator: ", ")
+            errStr += ": \(argsStr)"
+        }
+        if let kwargs = kwargs, !kwargs.isEmpty {
+            let kwargsStr = kwargs.map { "\($0.key)=\($0.value)" }.joined(separator: ", ")
+            errStr += ": \(kwargsStr)"
+        }
+        return errStr
+    }
+}
+
+class ProtocolError: Swift.Error {
+    let message: String
+
+    init(message: String) {
+        self.message = message
+    }
+}
+
+class SessionNotReady: Swift.Error {
+    let message: String
+
+    init(message: String) {
+        self.message = message
+    }
+}
